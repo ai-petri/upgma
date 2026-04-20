e997dffe2a28a57bd0ac80d43d3ff6f940d35f5a
Dmitry <234518@mail.ru>
2026-04-20 16:36:35 +0300

    fix ES6

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { getBrayCurtis } from "./BrayCurtis.js";
 import { createMatrix, buildTree } from "./tree.js";
 
-export { getBrayCurtis, createMatrix, buildTree };
+export default { getBrayCurtis, createMatrix, buildTree };
